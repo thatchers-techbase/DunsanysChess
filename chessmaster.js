9f7e139e51a8521123dad09cb5fc982708f9eda5
thatchers-techbase <88593304+thatchers-techbase@users.noreply.github.com>
2023-03-25 16:59:19 +0000

    Fix for pawn promotion issue

--- chessmaster.js
+++ chessmaster.js
@@ -548,16 +548,18 @@ var Chess = function (fen) {
   function generate_moves(options) {
     function add_move(board, moves, from, to, flags) {
       /* if pawn promotion */
-      if (
-        (board[from].type === PAWN || undefined) &&
-        (rank(to) === RANK_8 || rank(to) === RANK_1)
-      ) {
-        var pieces = [QUEEN, ROOK, BISHOP, KNIGHT];
-        for (var i = 0, len = pieces.length; i < len; i++) {
-          moves.push(build_move(board, from, to, flags, pieces[i]));
+      if (board[from]) {
+        if (
+          board[from].type === PAWN &&
+          (rank(to) === RANK_8 || rank(to) === RANK_1)
+        ) {
+          var pieces = [QUEEN, ROOK, BISHOP, KNIGHT];
+          for (var i = 0, len = pieces.length; i < len; i++) {
+            moves.push(build_move(board, from, to, flags, pieces[i]));
+          }
+        } else {
+          moves.push(build_move(board, from, to, flags));
         }
-      } else {
-        moves.push(build_move(board, from, to, flags));
       }
     }
 
